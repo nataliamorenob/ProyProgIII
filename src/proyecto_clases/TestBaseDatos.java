3106f5cbab301c7ae55c426dfc97102e0c0ef069
veronicagut <veronicagutierrez@opendeusto.es>
2021-12-29 13:38:42 +0100

    JUnit ANGELA

--- src/proyecto_clases/TestBaseDatos.java
+++ src/proyecto_clases/TestBaseDatos.java
@@ -6,13 +6,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class TestBaseDatos {
+	private Connection con;
 	
 	@Before
 	public void before() {
-		BD.initBD("BaseDatos.db");
+		con = BD.initBD("BaseDatos.db");
 	}
 	
 	@After
@@ -45,8 +48,22 @@ public class TestBaseDatos {
 		ArrayList<Perros> alPerros=BD.obtenerPerros(Connection con);
 		assertTrue(alPerros.size()==15);
 	}*/
-
 	
-	
+	@Test
+	public void testObtenerPerros(){
+		ArrayList<Perros> ap = BD.obtenerPerros(con);
+		ArrayList<String> l = new ArrayList<String>();
+		l.add("Beasain");
+		l.add("Vitoria");
+		l.add("Getxo");
+		int neg = -1;
+		int anyos = 20;
+		for (Perros p : ap){
+			assertTrue(l.contains(p.getLocalizacion()));
+			assertTrue(p.getPeso() > neg);
+			assertTrue(p.getTiempoEnAdopcion() > neg && p.getTiempoEnAdopcion() <= anyos );
+			
+		}
+	}
 	
 }
