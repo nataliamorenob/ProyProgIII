24e3d0bbc902143fdb0ca558108cdd99f9309941
aranllodio <aranllodio@opendeusto.es>
2021-12-29 17:20:33 +0100

    Cambio

--- src/proyecto_clases/PanelAccesorios.java
+++ src/proyecto_clases/PanelAccesorios.java
@@ -30,7 +30,7 @@ public class PanelAccesorios extends JPanel {
 		JPanel panelSur = new JPanel();
 		add(panelSur, BorderLayout.SOUTH);
 		
-		JButton btnComprar = new JButton("AÑADIR A LA CESTA");
+		JButton btnComprar = new JButton("COMPRAR");
 		btnComprar.setFont(new Font("Bodoni MT", Font.PLAIN, 11));
 		btnComprar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
